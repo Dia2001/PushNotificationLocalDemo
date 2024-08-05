5653c6f9fa34ed703adc5a99bae028a4e497ef2f
nguyenvandiatlvn3@gmail.com <nguyenvandiatlvn3@gmail.com>
2024-08-05 10:36:53 +0700

    feat: show notification

--- PushNotificationLocalDemo/MainPage.xaml.cs
+++ PushNotificationLocalDemo/MainPage.xaml.cs
@@ -1,24 +1,30 @@
-﻿namespace PushNotificationLocalDemo
+﻿using Plugin.LocalNotification;
+
+namespace PushNotificationLocalDemo
 {
     public partial class MainPage : ContentPage
     {
         int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
-
         private void OnCounterClicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            var request = new NotificationRequest
+            {
+                NotificationId = 1000,
+                Title = "Subscribe for me",
+                Subtitle = "Hello Friends",
+                Description = "Stay Tuned",
+                BadgeNumber = 42,
+                Schedule = new NotificationRequestSchedule
+                {
+                    NotifyTime = DateTime.Now.AddSeconds(5),
+                    NotifyRepeatInterval = TimeSpan.FromDays(1)
+                }
+            };
+            LocalNotificationCenter.Current.Show(request);
         }
     }
 
