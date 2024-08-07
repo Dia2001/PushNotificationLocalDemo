25e4d1e2726f6997a9bfe69aca035fdc50148d6d
nguyenvandiatlvn3@gmail.com <nguyenvandiatlvn3@gmail.com>
2024-08-07 10:30:30 +0700

    feat: success

--- PushNotificationLocalDemo/MainPage.xaml.cs
+++ PushNotificationLocalDemo/MainPage.xaml.cs
@@ -11,17 +11,26 @@ namespace PushNotificationLocalDemo
         }
         private void OnCounterClicked(object sender, EventArgs e)
         {
+            var medicationName = medicationNameEntry.Text;
+            var reminderTime = reminderTimePicker.Time;
+            var notifyTime = DateTime.Today.Add(reminderTime) < DateTime.Now ? DateTime.Today.AddDays(1).Add(reminderTime) : DateTime.Today.Add(reminderTime);
+            DisplayAlert("Thông báo", reminderTime.ToString() + " " + DateTime.Now, "OK");
             var request = new NotificationRequest
             {
-                NotificationId = 1000,
-                Title = "Subscribe for me",
-                Subtitle = "Hello Friends",
+                NotificationId = Guid.NewGuid().GetHashCode(),
+                Title = "Hẹn giờ uống thuốc",
+                Subtitle = "Đã đến h uống thuốc",
                 Description = "Stay Tuned",
                 BadgeNumber = 42,
-                Schedule = new NotificationRequestSchedule
+                /*Schedule = new NotificationRequestSchedule
                 {
                     NotifyTime = DateTime.Now.AddSeconds(5),
                     NotifyRepeatInterval = TimeSpan.FromDays(1)
+                }*/
+                Schedule = new NotificationRequestSchedule
+                {
+                    NotifyTime = notifyTime,
+                    RepeatType = NotificationRepeat.Daily // Thiết lập thông báo lặp lại hàng ngày
                 }
             };
             LocalNotificationCenter.Current.Show(request);
